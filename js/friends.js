b4333ef711f04631602cfb1a62a12374313ad9f1
AraceliCumen <aycuevam@gmail.com>
2018-01-17 11:10:28 -0500

    Actualizando  gh-pages

--- js/friends.js
+++
@@ -1,7 +0,0 @@
-// INCIALIZAMOS EL MENU HAMBURGUESA
-$(document).ready(function() {
-  $('.button-collapse').sideNav();
-  $('.foto-user').attr('src', localStorage.photo);
-  $('.name').text(localStorage.name);
-  $('.email').text(localStorage.mail);
-});  
